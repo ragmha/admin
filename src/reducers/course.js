6dca62f1c40f7e2e1357624e61333b472d2cf8d2
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-10 23:11:13 +0300

    add types

--- src/reducers/course.js
+++ src/reducers/course.js
@@ -1,6 +1,8 @@
+import { CREATE_COURSE } from "../actions/types";
+
 export default function course(state = [], action) {
   switch (action.type) {
-    case "CREATE_COURSE":
+    case CREATE_COURSE:
       return [...state, Object.assign({}, state, action.course)];
     default:
       return state;
