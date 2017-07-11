dd2fdb1cef0969b5b231455860ffc8116e4fa6ab
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-11 13:18:03 +0300

    add Loading dots component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from "react";
 import { Link, IndexLink } from "react-router";
+import LoadingDots from "./LoadingDots";
 
 const Header = () =>
   <nav>
@@ -14,6 +15,7 @@ const Header = () =>
     <Link to="/about" activeClassName="active">
       About
     </Link>
+    <LoadingDots interval={100} dots={20} />
   </nav>;
 
 export default Header;
