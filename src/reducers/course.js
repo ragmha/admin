2d2e7b782c9406c145bd030a260be2626edde0e1
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-07-11 12:51:55 +0300

    add course reducer

--- src/reducers/course.js
+++ src/reducers/course.js
@@ -1,12 +1,23 @@
-import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "../actions/types";
+import {
+  CREATE_COURSE_SUCCESS,
+  UPDATE_COURSE_SUCCESS,
+  LOAD_COURSES_SUCCESS
+} from "../actions/types";
 import initialState from "./initialState";
 
 export default function course(state = initialState.courses, action) {
   switch (action.type) {
     case LOAD_COURSES_SUCCESS:
       return action.courses;
-    case CREATE_COURSE:
-      return [...state, Object.assign({}, state, action.course)];
+
+    case CREATE_COURSE_SUCCESS:
+      return [...state, Object.assign({}, action.course)];
+
+    case UPDATE_COURSE_SUCCESS:
+      return [
+        ...state.filter(course => course.id !== action.course.id),
+        Object.assign({}, action.course)
+      ];
     default:
       return state;
   }
